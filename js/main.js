f750f27a0e6aa30347f2d3f84222a1f3153a2a32
Jason You <jasonyou92@gmail.com>
2017-08-05 15:29:08 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function addComment() {
+	var name = document.getElementById("name").value
+	var nameText = document.createTextNode(name)
+	var comment = document.getElementById("comment").value
+	var commentText = document.createTextNode(comment)
+	var newName = document.createElement("h1")
+	newName.className = "username"
+	newName.className = "animated slideInUp"
+	var newComment = document.createElement("p")
+	newComment.className = "usercomment"
+	newComment.className = "animated slideInUp"
+	newName.appendChild(nameText)
+	newComment.appendChild(commentText)
+	document.getElementById("body").appendChild(newName)
+	document.getElementById("body").appendChild(newComment)
+}
